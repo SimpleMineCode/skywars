89af31ba399d6da576c1ed70b90c03089768320a
SMCode <hello@smcode.io>
2025-07-28 14:22:15 +0200

    Finish part 1

--- src/main/java/io/smcode/skywars/game/GameManager.java
+++ src/main/java/io/smcode/skywars/game/GameManager.java
@@ -1,15 +1,10 @@
 package io.smcode.skywars.game;
 
 import io.smcode.skywars.SkyWarsPlugin;
-import org.bukkit.Location;
-import org.bukkit.configuration.ConfigurationSection;
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.UUID;
 
 public class GameManager {
-    private final Set<Game> games = new HashSet<>();
     private final SkyWarsPlugin plugin;
 
     public GameManager(SkyWarsPlugin plugin) {
@@ -27,8 +22,9 @@ public class GameManager {
         saveGameToConfig(game);
         return game;
     }
+
     private void saveGameToConfig(Game game) {
-        plugin.getConfig().set("games." + game.getId(), game);
+        plugin.getConfig().set("games." + UUID.randomUUID(), game);
         plugin.saveConfig();
     }
 }
